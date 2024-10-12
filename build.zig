88187c279698154e253af0a00d853661f4c37e53
Tai Kedzierski <dch.tai@gmail.com>
2024-10-13 00:50:02 +0100

    Fix build.zig errors

--- build.zig
+++ build.zig
@@ -1,12 +1,12 @@
 const std = @import("std");
 
-pub fun build(b: *std.Build) void {
+pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
-    const.optimize = b.standardOptimizeOption(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("zig-linereader", .{
         .root_source_file = b.path("src/linereader.zig"),
-        .target = targt,
+        .target = target,
         .optimize = optimize,
     });
 }
